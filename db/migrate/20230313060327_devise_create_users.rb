cebcdded320ddceb9796ff38d5e01497557e9066
yowagai <imlf4btbw28@gmail.com>
2023-03-13 15:29:43 +0900

    DB：カラム名作成

--- db/migrate/20230313060327_devise_create_users.rb
+++ db/migrate/20230313060327_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :child_name,         null: false
+      t.string  :child_name_kana,    null: false
+      t.string  :parent_name,        null: false
+      t.string  :parent_name_kana,   null: false
+      t.integer :sex_id,             null: false
+      t.date    :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
