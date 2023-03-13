ed4d3a4109a8180331a8ed93deb942a711e4a84e
yowagai <imlf4btbw28@gmail.com>
2023-03-13 17:44:23 +0900

    アクティブハッシュ導入
    フィールド編集

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ group :production do
 end
 
 gem 'devise'
+gem 'active_hash'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sex
+
+  validates :sex_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+
 end
