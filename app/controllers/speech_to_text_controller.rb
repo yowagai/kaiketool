893292b835937c6c33cde116ed00bd897c9c44f9
yowagai <imlf4btbw28@gmail.com>
2023-03-15 07:57:35 +0900

    speech-to-texi apiの非同期変換

---
+++ app/controllers/speech_to_text_controller.rb
@@ -0,0 +1,30 @@
+class SpeechToTextController < ApplicationController
+  def index
+    require "google/cloud/speech"
+    speech = Google::Cloud::Speech.speech
+
+    storage_path = "gs://kaiketool/test.flac"
+
+    config = { encoding:          :FLAC,    #FLAC、LINEAR16が対応
+               sample_rate_hertz: 44_100,   #サンプリングレート
+               language_code:     "ja-JP" } #日本語に設定
+    audio = { uri: storage_path }
+
+    operation = speech.long_running_recognize config: config, audio: audio
+
+    puts "Operation started"
+
+    operation.wait_until_done!
+
+    raise operation.results.message if operation.error?
+
+    results = operation.response.results
+
+    @transcriptions = "Transcription: "
+    results.each do |result|
+      alternatives = result.alternatives
+      @transcriptions += "#{alternatives.first.transcript}"
+    end
+    # [END speech_transcribe_async_gcs]
+  end
+end
