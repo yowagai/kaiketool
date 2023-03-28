8ca138f9c43a67d539bf3a7f9573c7549e0791fa
yowagai <imlf4btbw28@gmail.com>
2023-03-29 00:06:38 +0900

    slick完了
    スライドショー動作確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,25 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../speech_to_text")
 
 import "jquery";
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+import 'slick-carousel';
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true, // 自動再生を有効にする場合
+    autoplaySpeed: 5000, // 自動再生のスピードを指定する場合
+    arrows: true, // 前後の矢印を表示する場合
+    dots: true, // ページネーションを表示する場合
+    speed: 500, // スライドのスピードを指定する場合
+    slidesToShow: 1, // 一度に表示するスライドの数を指定する場合
+    slidesToScroll: 1 // スライドをスクロールする数を指定する場合
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
