cb847fac37e113941cb7f73f6a13156d2b92ca87
yowagai <imlf4btbw28@gmail.com>
2023-03-31 04:23:54 +0900

    トップ編集、検索結果画面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,7 +18,7 @@ import 'slick-carousel';
 $(document).ready(function(){
   $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
-    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    autoplaySpeed: 4000,//次のスライドに切り替わる待ち時間
     speed:1000,//スライドの動きのスピード。初期値は300。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
