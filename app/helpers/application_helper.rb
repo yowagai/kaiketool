65e3684beff936717e85b450e238e06c97469fe8
yowagai <imlf4btbw28@gmail.com>
2023-03-31 07:00:33 +0900

    セレクトフォーム編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def birth_date_select(form, method)
+    form.date_select method,
+      {
+        start_year: 2005,
+        end_year: Time.now.year,
+        use_month_numbers: true,
+        prompt: { year: '年', month: '月', day: '日' }
+      },
+      { class: 'birth-date-select' }
+  end
 end
