3d84d1c88e79aa19347caa11603a8b2a97d1628a
yowagai <imlf4btbw28@gmail.com>
2023-04-16 17:17:10 +0900

    ユーザー情報取得
    他の画面の情報取得もできるように

--- app/controllers/kaiketools_controller.rb
+++ app/controllers/kaiketools_controller.rb
@@ -1,7 +1,10 @@
 class KaiketoolsController < ApplicationController
   def index
-    @search_history = SearchHistory.first
-    @search_histories = SearchHistory.all
+    if user_signed_in?
+      @search_history_user = SearchHistory.find_by(user_id: current_user.id)
+    else
+      @search_history_user = nil
+    end
   end
 
   # def search
