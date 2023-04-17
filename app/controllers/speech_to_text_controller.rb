f7a4e3eb6eb8d6f316f0561bce66c57826b772d9
yowagai <imlf4btbw28@gmail.com>
2023-04-17 22:24:36 +0900

    プロフィールcss編集

--- app/controllers/speech_to_text_controller.rb
+++ app/controllers/speech_to_text_controller.rb
@@ -6,34 +6,38 @@ class SpeechToTextController < ApplicationController
   end
 
   def search
+    speech_text
+  end
+  
+  private
+  
+  def speech_text
     require "google/cloud/speech"
     speech = Google::Cloud::Speech.speech
-
+  
     storage_path = "gs://kaiketool/airpline.flac"
-
+  
     config = { encoding:          :FLAC,    #FLAC、LINEAR16が対応
               sample_rate_hertz: 44_100,   #サンプリングレート
               language_code:     "ja-JP" } #日本語に設定
     audio = { uri: storage_path }
-
+  
     operation = speech.long_running_recognize config: config, audio: audio
-
+  
     puts "Operation started"
-
+  
     operation.wait_until_done!
-
+  
     raise operation.results.message if operation.error?
-
+  
     results = operation.response.results
-
+  
     @transcriptions = ""
     results.each do |result|
       alternatives = result.alternatives
       @transcriptions += "#{alternatives.first.transcript}"
     end
   end
-
-  private
 
   def set_user
     if user_signed_in?
